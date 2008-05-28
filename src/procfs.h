72dc25b463a953fda06e98a69f2274a24315ebb9
Madhusudan.C.S <madhusudancs@gmail.com>
2008-05-28 20:14:31 +0530

    procfs.h -- a header file containing major definitions for the procfs translator is added.

---
+++ src/procfs.h
@@ -0,0 +1,30 @@
+/* procfs -- a translator for providing GNU/Linux compatible 
+             proc pseudo-filesystem
+
+   procfs.h -- This file is the main header file of this
+               translator. This has important header 
+               definitions for constants and functions 
+               used in the translator.
+               
+   Copyright (C) 2008, FSF.
+   Written as a Summer of Code Project
+   
+   procfs is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   procfs is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. 
+*/
+
+#define PROCFS_SERVER_NAME "procfs"
+#define PROCFS_SERVER_VERSION "0.0.1"
+
+

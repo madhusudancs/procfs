43afe2f4b7df82fc4e73a405023ecef906813952
Madhusudan.C.S <madhusudancs@gmail.com>
2008-06-01 11:37:22 +0530

    Added netfs callbacks required for creating empty directories using netfs. Added trivial callbacks

--- src/netfs.c
+++ src/netfs.c
@@ -19,7 +19,53 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. 
 */
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <hurd/netfs.h>
 #include <sys/stat.h>
-#include <errno.h>
-#include <fcntl.h>
+
+#include "procfs.h"
+
+/* Trivial definitions.  */
+
+/* Make sure that NP->nn_stat is filled with current information.  CRED
+   identifies the user responsible for the operation.  */
+error_t
+netfs_validate_stat (struct node *node, struct iouser *cred)
+{
+  return procfs_refresh_node (node);
+}
+
+/* This should sync the file NODE completely to disk, for the user CRED.  If
+   WAIT is set, return only after sync is completely finished.  */
+error_t
+netfs_attempt_sync (struct iouser *cred, struct node *node, int wait)
+{
+  return 0;
+}
+
+/* Attempt to create a new directory named NAME in DIR for USER with mode
+   MODE.  */
+error_t netfs_attempt_mkdir (struct iouser *user, struct node *dir,
+			     char *name, mode_t mode)
+{
+  return EROFS;
+}
+
+/* Attempt to remove directory named NAME in DIR for USER. */
+error_t netfs_attempt_rmdir (struct iouser *user,
+			     struct node *dir, char *name)
+{
+  return EROFS;
+}
+
+/* Attempt to set the passive translator record for FILE to ARGZ (of length
+   ARGZLEN) for user CRED. */
+error_t netfs_set_translator (struct iouser *cred, struct node *node,
+			      char *argz, size_t argzlen)
+{
+  return EROFS;
+}

0f032238de0e71723b57a79a2e9439af1e0852a7
Madhusudan.C.S <madhusudancs@gmail.com>
2008-06-04 01:36:09 +0530

    bootstrap.c - file containing the functions necessary to setup the filesystem and the translator, and also function for procfs inialization Added License and Copyright information to the file. Added headers to the file. Added function definitions and stubs for functions procfs_init and procfs_create

---
+++ src/bootsrap.c
@@ -0,0 +1,44 @@
+/* procfs -- a translator for providing GNU/Linux compatible 
+             proc pseudo-filesystem
+
+   bootstrap.c -- This file is functions for starting up 
+                  and initializers for the procfs translator
+                  defined in procfs.h
+               
+   Copyright (C) 2008, FSF.
+   Written as a Summer of Code Project
+   
+   procfs is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   procfs is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. 
+*/
+
+/* This function is used to initialize the whole translator, can be
+   effect called as bootstrapping the translator. */
+void procfs_init ()
+{
+
+    /*  STUB  */
+    
+}
+
+/* Create a new procfs filesystem.  */
+error_t procfs_create (struct procfs **fs)
+{
+
+     /*  STUB  */
+     
+  return 0;
+}
+
+

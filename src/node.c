dd812f0ba62a0b99c59782097d84b9dee9390714
Madhusudan.C.S <madhusudancs@gmail.com>
2008-06-05 11:08:28 +0530

    Added header includes for <hurd/ihash.h>, <hurd/fshelp.h>   and <hurd/iohelp.h> Changed the incorrect function head for procfs_node_create   to match with its prototype Added definition to this function to create new nodes

--- src/node.c
+++ src/node.c
@@ -26,18 +26,55 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <hurd/ihash.h>
+#include <hurd/fshelp.h>
+#include <hurd/iohelp.h>
 
 #include <hurd/netfs.h>
 
 #include "procfs.h"
 
-/* Return a new node in NODE, with a name NAME, and return the new node
-   with a single reference in NODE. */
-error_t procfs_create_node (struct node **node)
+/* Return a new node in NODE, with a name NAME, and return the
+   new node with a single reference in NODE. */
+error_t procfs_create_node (struct procfs_dir_entry *dir_entry, 
+                            const char *fs_path, struct node **node)
 {
+  struct node *new;
+  struct netnode *nn = malloc (sizeof (struct netnode));
+  error_t err;
 
-    /* STUB */
-    
+  if (! nn)
+    return ENOMEM;
+
+  nn->fs = dir_entry->dir->fs;
+  nn->dir_entry = dir_entry;
+  nn->dir = NULL;
+  nn->fs_path = strdup (fs_path);
+
+  new = netfs_make_node (nn);
+  if (! new)
+    {
+      free (nn);
+      return ENOMEM;
+    }
+
+  fshelp_touch (&new->nn_stat, TOUCH_ATIME|TOUCH_MTIME|TOUCH_CTIME,
+		procfs_maptime);
+
+  spin_lock (&nn->fs->inode_mappings_lock);
+  err = hurd_ihash_add (&nn->fs->inode_mappings, dir_entry->stat.st_ino, dir_entry);
+  spin_unlock (&nn->fs->inode_mappings_lock);
+
+  if (err)
+    {
+      free (nn);
+      free (new);
+      return err;
+    }
+
+  dir_entry->node = new;
+  *node = new;
+
   return 0;
 }
 

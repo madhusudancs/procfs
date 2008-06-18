0518b62ebc33877052c803e13ddb2a49c5fb8adb
Madhusudan.C.S <madhusudancs@gmail.com>
2008-06-19 02:20:50 +0530

    Removed double initializations of global variables and pointers and call to bootstrap root node
    Due to a small negligency there was redundant code which used to initialize
    all the global variables and pointers twice, these were removed from procfs_init
    function.
    A call to the function procfs_dir_null_lookup which is used to bootstrapping the
    root node was added to procfs_create function.

--- src/bootstrap.c
+++ src/bootstrap.c
@@ -36,32 +36,9 @@ static struct ps_context *ps_context;
 error_t procfs_init ()
 {
   error_t err;
-  struct procfs_dir **procfs_root_dir;
-  struct procfs *procfs_root_fs;
-  struct *node;
   
   err = ps_context_create (getproc (), &ps_context);
   
-  procfs_root_fs = (procfs *) malloc (sizeof (procfs));
-  procfs_root_dir = (procfs_dir **) malloc (sizeof (procfs_dir *));
-  
-  if (!procfs_root_fs || !procfs_root_dir)
-    return ENOMEM;
-  
-  procfs_root_fs->root = NULL;
-  procfs_root_fs->next_inode = 0;
-  procfs_root_fs->fsid = 0;
-  
-  if (! err) 
-    {
-      /* Create Root of proc filesystem. */
-      err = procfs_dir_create (procfs_root_fs, node, "", procfs_root_dir);
-      if (! err)
-         /* Create all the directories of name PID 
-            for procfs root. */
-         err = procfs_dir_refresh (*procfs_root_dir);
-    }
-   
   return err; 
 }
 
@@ -73,7 +50,7 @@ error_t procfs_create (char *procfs_root, int fsid,
   /* This is the enclosing directory for this filesystem's
      root node  */
   struct procfs_dir *topmost_root_dir;
-  
+ 
   /* And also a topmost-root node, just used for locking
      TOPMOST_ROOT_DIR.  */
   struct node *topmost_root;
@@ -100,8 +77,8 @@ error_t procfs_create (char *procfs_root, int fsid,
                                &topmost_root_dir);
       if (! err)
         {
-          /* ADDITIONAL BOOTSTRAPPING OF THE ROOT NODE MAYBE REQUIRED
-	     LEFT AS A STUB  */
+          /* ADDITIONAL BOOTSTRAPPING OF THE ROOT NODE */
+	  err = procfs_dir_null_lookup (topmost_root_dir, &new->root);   
         }
     }
 
@@ -115,5 +92,4 @@ error_t procfs_create (char *procfs_root, int fsid,
 
   return err;
 }
-
 

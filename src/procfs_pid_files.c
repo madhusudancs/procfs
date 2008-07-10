b4a45a7c199b657c832fe6a0227affdc15149001
Madhusudan.C.S <madhusudancs@gmail.com>
2008-07-11 03:11:28 +0530

    Added header includes and definition for functions set_field_value, get_stat_data, procfs_write_stat_file and procfs_write_files_contents
    Added header includes for <string.h>, <stdio.h>, "procfs_pid.h"
    In function procfs_write_files_contents - Added call to procfs_write_stat_file for
      the condition when the name of the file for which contents is to be constructed
      is "stat".
    Added definition for function procfs_write_stat_file.
    Added definition for function get_stat_data.
    Added definition for function set_field_value.

--- src/procfs_pid_files.c
+++ src/procfs_pid_files.c
@@ -32,8 +32,11 @@
 
 #include <hurd/netfs.h>
 #include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "procfs.h"
+#include "procfs_pid.h"
 
 /* Update the files named NAME within the directory named
    PID also with SYMLINK TARGET if necessary. */
@@ -83,15 +86,140 @@ procfs_create_files (struct procfs_dir *dir,
   return err;
 }
 
-/* Writes required process information to each of 
-   files within DIR, with name pid for files specified
-   by NODE. */
-error_t 
-procfs_write_files_contents (struct node **node, time_t timestamp)
+/* Check if the PSTAT_FLAG is set in the corresponding PS
+   structure, if not set it and check again and return error
+   status accordingly. */
+error_t set_field_value (struct proc_stat *ps, int pstat_flag)
 {
-  int err;
+  error_t err;
+
+  if (! (ps->flags & pstat_flag))
+    {
+      err = proc_stat_set_flags (ps, pstat_flag);
+      if (err)
+        return err;
+
+      /* This second check is done since ps.h specifies to
+         do so since the previous call would not have set
+         the required value. */
+      if (! (ps->flags & pstat_flag))
+        return EGRATUITOUS;
+    }
+
+  return 0;
+}
+
+/* Get the data for stat file into the structure
+   PROCFS_STAT. */
+error_t get_stat_data (pid_t pid, 
+                       struct procfs_stat **procfs_stat)
+{
+  error_t err;
+  struct procfs_stat *new = (struct procfs_stat *)
+                    malloc (sizeof (struct procfs_stat));
+
+  struct proc_stat *ps;
+  err = _proc_stat_create (pid, ps_context, &ps);
+
+  new->pid = pid;
+
+  if (! err)
+    {
+      err = set_field_value (ps, PSTAT_ARGS);
+      if (! err)
+        asprintf (&new->comm, "(%s)", ps->args);
+    }
+  if (! err)
+    {
+      err = set_field_value (ps, PSTAT_STATE);
+      if (! err)
+        {
+          if (ps->state & PSTAT_STATE_P_STOP) 
+            new->state = strdup ("T");
+          if (ps->state & PSTAT_STATE_P_ZOMBIE)
+            new->state = strdup ("Z");
+          if (ps->state & PSTAT_STATE_P_FG)
+            new->state = strdup ("+");
+          if (ps->state & PSTAT_STATE_P_SESSLDR)
+            new->state = strdup ("s");
+          if (ps->state & PSTAT_STATE_P_LOGINLDR)
+            new->state = strdup ("l");
+          if (ps->state & PSTAT_STATE_P_FORKED)
+            new->state = strdup ("f");
+          if (ps->state & PSTAT_STATE_P_NOMSG)
+            new->state = strdup ("m");
+          if (ps->state & PSTAT_STATE_P_NOPARENT)
+            new->state = strdup ("p");
+          if (ps->state & PSTAT_STATE_P_ORPHAN)
+            new->state = strdup ("o");
+          if (ps->state & PSTAT_STATE_P_TRACE)
+            new->state = strdup ("x");
+          if (ps->state & PSTAT_STATE_P_WAIT)
+            new->state = strdup ("w");
+          if (ps->state & PSTAT_STATE_P_GETMSG)
+            new->state = strdup ("g");     
+        }
+    }
+  if (! err)
+    {
+      err = set_field_value (ps, PSTAT_PROC_INFO);
+      if (! err)
+        {
+          new->ppid = ps->proc_info->ppid;
+          new->pgid = ps->proc_info->pgrp;
+          new->sid = ps->proc_info->session;
+        }
+    }
+
+  *procfs_stat = new;
+  _proc_stat_free (ps);
+
+  return err;
+}
+
+/* Writes required process information to stat file
+   within the directory represented by pid. Return
+   the data in DATA and actual length to be written
+   in LEN. */
+error_t
+procfs_write_stat_file (struct procfs_dir_entry *dir_entry, 
+                        off_t offset, size_t *len, void *data)
+{
+  error_t err;
+  char *stat_data;
+  struct procfs_stat *procfs_stat;
+  pid_t pid = atoi (dir_entry->dir->node->nn->dir_entry->name);
+
+  err = get_stat_data (pid, &procfs_stat);
   
-         /*  STUB  */
+  if (asprintf (&stat_data, "%d %s %s %d %d %d\n", procfs_stat->pid,
+                   procfs_stat->comm, procfs_stat->state,
+                   procfs_stat->ppid, procfs_stat->pgid,
+                   procfs_stat->sid) == -1)
+    return errno;
+
+  memcpy (data, stat_data, strlen(stat_data));
+  *len = strlen (data);
   
+  free (stat_data);
+  free (procfs_stat);
+  
+  return err;  
+}
+
+/* Writes required process information to each of files
+   within directory represented by pid, for files specified
+   by NODE. Return the data in DATA and actual length of 
+   data in LEN. */
+error_t 
+procfs_write_files_contents (struct node *node,
+                 off_t offset, size_t *len, void *data)
+{
+  error_t err;
+
+  if (! strcmp (node->nn->dir_entry->name, "stat"))
+    err = procfs_write_stat_file (node->nn->dir_entry, 
+                                      offset, len, data);
+
   return err;
 }
